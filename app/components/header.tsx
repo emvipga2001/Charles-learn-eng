3e9e213810570e084ba8c3be2ae44e2c9d6975bf
Charles_CTY <nguyenhongminhquan25@gmail.com>
2024-08-29 17:26:47 +0700

    CLE_005: Login page

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,17 +1,23 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from "next/link";
 import Logo from "../../public/Logo.png"
 import { ModeToggle } from '../../ui/button-dark-mode';
 import Image from 'next/image';
 import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
   const [activeLink, setActiveLink] = useState('');
+  const pathname = usePathname();
 
+  console.log(pathname);
   return (
-    <div className='flex bg-white dark:bg-black justify-between px-4'>
+    <div className={clsx(
+      'flex bg-white dark:bg-black justify-between px-4',
+      pathname == "/login" && 'hidden'
+    )}>
       <div className='content-center'>
         <Image src={Logo} alt={'Logo'} width={90} priority={true} />
       </div>
